a493cc502a9cf7f5251ed20ece8c2fd2090b0dfe
tomtom2211 <tomtom2211@seznam.cz>
2025-06-28 20:23:55 +0200

    Test

---
+++ src/main/java/com/tomtom2211/modid/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.tomtom2211.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/tomtom2211/modid/ExampleModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tomtom2211.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
