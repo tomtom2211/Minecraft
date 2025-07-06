6906047b716b521b11c7dc0bd09252160877fa99
tomtom2211 <tomtom2211@seznam.cz>
2025-07-06 15:19:01 +0200

    Night Vision Command

--- src/main/java/com/tomtom2211/modid/ExampleModClient.java
+++ src/main/java/com/tomtom2211/modid/ExampleModClient.java
@@ -5,6 +5,17 @@ import net.fabricmc.api.ClientModInitializer;
 public class ExampleModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        KeyBinding NightVision = KeyBindingHelper.registerKeyBinding(new KeyBinding("Apply/Remove night vision", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, "tomtom2211 mod"));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (NightVision.wasPressed()) {
+                if (enabled){
+                    client.player.networkHandler.sendChatCommand("effect give @s night_vision 60 1");
+                }
+                else {
+                    client.player.networkHandler.sendChatCommand("effect clear");
+                }
+                enabled = !enabled;
+            }
+        });
     }
 }
